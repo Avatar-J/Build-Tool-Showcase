f7c513439e946ad65bed5fe814e0ab1b7dc2781e
Jummai <jummyhassan5@gmail.com>
2025-05-28 17:42:37 +0000

    setup project and installed required packages

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const fname: string = "jummai";
+
+console.log(`Hi! I am Jummai ${fname}`);
+
+const para = document.createElement("p");
+para.innerHTML = "show me the way senior developer";
+document.body.appendChild(para);
---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: "css-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "public"),
+    clean: true,
+  },
+};
