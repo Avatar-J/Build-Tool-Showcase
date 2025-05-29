324acff9f87bbc71e9f523a4d17456f329b0a3c5
Jummai <jummyhassan5@gmail.com>
2025-05-29 10:34:41 +0000

    added the dark theme switch functionality

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import "./style.scss";
 import * as packageJson from "../package.json";
 
-const mainContainer = document.getElementById("mainContainer");
+const mainContainer = document.getElementById("tool-section");
+const themeSwitchBtn = document.getElementById("toggleTheme");
 
-console.log(packageJson.devDependencies);
+function createToolElement(data: string, element: HTMLElement) {
+  const listContainer = document.createElement("div");
+  listContainer.classList.add("tool-container");
 
-function createListElement(data: string, element: HTMLElement) {
-  const list = document.createElement("li");
+  const list = document.createElement("h2");
   list.textContent = data;
 
-  if (list) {
-    element.appendChild(list);
+  if (listContainer && list) {
+    listContainer.appendChild(list);
+    element.appendChild(listContainer);
   }
 }
 
@@ -18,8 +21,16 @@ function displayTools(data: { [key: string]: string }, element: HTMLElement) {
   const toolName = Object.keys(data);
 
   toolName.forEach((tool) => {
-    createListElement(tool, element);
+    createToolElement(tool, element);
+  });
+}
+
+function addListenerToThemeSwitch(element: HTMLElement) {
+  element.addEventListener("click", () => {
+    console.log("theme");
+    document.body.classList.toggle("darkTheme");
   });
 }
 
 displayTools(packageJson.devDependencies, mainContainer as HTMLElement);
+addListenerToThemeSwitch(themeSwitchBtn as HTMLElement);
