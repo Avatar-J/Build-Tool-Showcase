f51ccb121c892c793ef255bd1f11764fc1413b01
Jummai <jummyhassan5@gmail.com>
2025-05-29 16:17:32 +0000

    installed moment

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-import "./style.scss";
-import * as tools from "../tools.json";
+import './style.scss';
+import * as tools from '../tools.json';
+import moment from 'moment';
 
-const mainContainer = document.getElementById("tool-section");
-const themeSwitchBtn = document.getElementById("toggleTheme");
+const mainContainer = document.getElementById('tool-section');
+const themeSwitchBtn = document.getElementById('toggleTheme');
 
 function createToolElement(
   data: { name: string; description: string },
   element: HTMLElement
 ) {
-  const listContainer = document.createElement("div");
-  listContainer.classList.add("tool-container");
+  const listContainer = document.createElement('div');
+  listContainer.classList.add('tool-container');
 
-  const title = document.createElement("h2");
+  const title = document.createElement('h2');
   title.textContent = data.name;
 
-  const desc = document.createElement("p");
+  const desc = document.createElement('p');
   desc.textContent = data.description;
 
   listContainer.appendChild(title);
@@ -32,11 +33,19 @@ function displayTools(
 }
 
 function addListenerToThemeSwitch(element: HTMLElement) {
-  element.addEventListener("click", () => {
-    console.log("theme");
-    document.body.classList.toggle("darkTheme");
+  element.addEventListener('click', () => {
+    console.log('theme');
+    document.body.classList.toggle('darkTheme');
   });
+}
+
+const year = moment().format('YYYY');
+const copyRight = document.getElementById('year');
+
+function updateCurrentYear(element: HTMLElement, year: string) {
+  element.textContent = year;
 }
 
 displayTools(tools, mainContainer as HTMLElement);
 addListenerToThemeSwitch(themeSwitchBtn as HTMLElement);
+updateCurrentYear(copyRight as HTMLElement, year);
--- webpack.common.js
+++ webpack.common.js
@@ -1,32 +1,32 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: './src/index.ts',
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.scss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: "bundle.[contenthash].js",
-    path: path.resolve(__dirname, "public"),
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'public'),
     clean: true,
   },
 };
